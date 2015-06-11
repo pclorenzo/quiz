c55474a5675c925ae670b86367182b5a2778dfdb
Pablo C. Lorenzo <plorenzo.uy@gmail.com>
2015-06-11 15:37:40 -0300

    Obligatorio Módulo 08 - correcciones

--- models/quiz.js
+++ models/quiz.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
 				validate: {notEmpty: {msg: "-> Falta Respuesta"}}
 			},
 			tema: {
-				type:  DataTypes.STRING,
+				type: DataTypes.STRING,
 				validate: {
 					notEmpty: {msg: "-> Falta Tema"},
 					isIn: {
